ea341883aff8b306aa0a762bfdddf1e8b068b09e
Carlos Andrés Pupiales Córdoba <capc3101@gmail.com>
2024-11-29 10:13:55 -0500

    terminando toolbar

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -9,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class SidenavComponent implements OnInit {
 
   mobileQuery: MediaQueryList;
+  menuNav = [
+    { name: "Home", route: "home", icon:"home"},
+    { name: "Categorias", route: "category", icon:"category"},
+    { name: "Productos", route: "product", icon:"production_quantity_limits"},
+  ]
+
   constructor(media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)')
   }
